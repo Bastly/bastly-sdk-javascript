305aa41c9fa7560e3f584de53b5b040d5247060f
gustavogiudici <gustavogiudici@gmail.com>
2015-03-31 11:39:05 +0000

    adds tests

---
+++ src/__tests__/tests.js
@@ -0,0 +1,51 @@
+var assert = require('chai').assert;
+var bastly = require('../page.js')();
+
+describe('Sanity check', function() {
+    it('tests work', function (done) {
+        assert.equal('200', '200');
+        done();
+    });
+});
+
+describe('Can request worker', function() {
+    it('requests a worker', function (done) {
+        var id = "fakeIdd";
+        bastly.getWorker(id, function(response){
+            done();
+        });
+    });
+});
+
+describe('Can send messages ', function() {
+    it('sends a message', function (done) {
+        var id = "fakeIdd";
+        bastly.getSend(id, msg, function(response){
+            //is there a response?
+            assert.not.null(response);  
+            done();
+        });
+    });
+});
+
+describe('Receives pings from worker ', function() {
+    it('requests a worker', function (done) {
+        var id = "fakeIdd";
+        bastly.onPing(function(response){
+            //is there a response?
+            assert.not.null(response);  
+            done();
+        });
+    });
+});
+
+describe('Receives messages', function() {
+    it('sends a message and receives it', function (done) {
+        var id = "fakeIdd";
+        bastly.on('message',  function(response){
+            //is there a response?
+            assert.not.null(response);  
+            done();
+        });
+    });
+});
