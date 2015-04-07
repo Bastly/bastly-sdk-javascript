4d5d0974be7c1d49d4fffdb27976ca64ac90b5a9
gustavogiudici <gustavogiudici@gmail.com>
2015-04-07 11:15:23 +0000

    adds comments and small improvementes

--- src/javascript/bastly.js
+++ src/javascript/bastly.js
@@ -1,17 +1,14 @@
 console.log('loaded');
 var _ = require('lodash');
 var request = require('request');
+var constants = require('bastly_constants');
 var bastly = {};
 var io = require('socket.io-client');
+var IP_ATAHUALPA = '192.168.0.112';
 bastly.workers = {};
 bastly.callbacks = {};
 
 var closeWorker = function(worker){
-    console.log('closing Worker', worker.ip);
-    console.log('closing Worker', worker.ip);
-    console.log('closing Worker', worker.ip);
-    console.log('closing Worker', worker.ip);
-    console.log('closing Worker', worker.ip);
     console.log('closing Worker', worker.ip);
     worker.socket.disconnect();
     bastly.workers[worker.ip] = {};
@@ -19,19 +16,19 @@ var closeWorker = function(worker){
 };
 
 var getWorker = function getWorker(channel, callback){
-    console.log('hola!');
-    request('http://192.168.0.112:8080/api/requestChaski?channel=' + channel + '&chaskiType=SOCKETIO', function (error, response, body) {
-        console.log('holaGato!');
-        console.log(response);
-        console.log(body);
+    console.log('getting worker!');
+    request('http://' + IP_ATAHUALPA + ':8080/api/requestChaski?channel=' + channel + '&chaskiType=' + constants.CHASKI_TYPE_SOCKETIO, function (error, response, body) {
+        console.log('Worker got!', body);
         var msg = JSON.parse(body);
         var workerIp = msg.message.ip;
-        bastly.workers[workerIp] = {};
-        bastly.workers[workerIp].channels = [];
+        //if it's a existing worker, recover it and continue working with it
+        bastly.workers[workerIp] = bastly.workers[workerIp] || {};
+        bastly.workers[workerIp].channels = bastly.workers[workerIp].channels || [];
         bastly.workers[workerIp].channels.push({channel:channel});
         bastly.workers[workerIp].ip = workerIp;
+        // forceNew is required because a connect/disconnect/connect cycle does not work without it
         bastly.workers[workerIp].socket = io.connect('http://' + msg.message.ip + ':3000', {'forceNew': true });
-        bastly.workers[workerIp].pingInterval = pingControl(bastly.workers[workerIp]);
+        bastly.workers[workerIp].pingInterval =  bastly.workers[workerIp].pingInterval || pingControl(bastly.workers[workerIp]);
         callback(bastly.workers[workerIp]);
     });
 };
@@ -39,10 +36,11 @@ var getWorker = function getWorker(channel, callback){
 bastly.send = function send(to, msg, callback){
     console.log('send', Date.now());
     request.post({
-            url:'http://192.168.0.112:8080/api/publishMessage', 
+            url:'http://' + IP_ATAHUALPA + ':8080/api/publishMessage', 
             form: {to: to, from: bastly.from, apiKey: bastly.apiKey, data:JSON.stringify(msg) }
         }, 
         function(err,httpResponse,body){ 
+            //ACK callback 
             if(callback){
                 callback(err, httpResponse, body);
             } 
@@ -51,24 +49,26 @@ bastly.send = function send(to, msg, callback){
 };
 
 var isAlive = function(worker){
+    //if alive, set isAlive to false, pings make it alive again
     if(worker.isAlive === true){
         console.log('worker:', worker.ip, "IT'S ALIVE!");
         worker.isAlive = false;
     } else {
-        console.log('worker:', worker.ip, "is dead... RIP ");
+        console.log('worker:', worker.ip, "is dead... RIP");
         closeWorker(worker);
         //need to replace the worker
         _.each(worker.channels, function(channel){
-            getWorker(channel, function(worker){
+            getWorker(channel, function(newWorker){
                 console.log('worker got');
                 //recovering previous channel callbacks
-                worker.socket.on(channel, function(data){
+                newWorker.socket.on(channel, function(data){
                     bastly.callbacks[channel](data);
                 });
             }); 
         });
     }
 };
+
 var pingControl = function(worker){
     worker.socket.on('ping', function(){
         console.log('gotPing, worker', worker.ip, 'LIVE LONG AND PROSPER');
@@ -77,6 +77,7 @@ var pingControl = function(worker){
     return setInterval(function() { isAlive(worker); }, 5000);
 };
 
+// for assigning new callbacks
 bastly.on = function on(id, callback){
     bastly.callbacks[id] = callback;
 };
@@ -90,17 +91,15 @@ bastly.subscribe = function(channel, channelCallback){
             bastly.callbacks[channel](data);
         });
     }); 
-
 };
-
 
 window.bastly = module.exports = function(from, apiKey, callback){
 
+    //TODO missing checks
     bastly.from = from;
     bastly.apiKey = apiKey;
     bastly.callbacks[bastly.from] = callback;
 
-    //TODO missing checks
     bastly.subscribe(bastly.from, callback);
 
     return bastly;
