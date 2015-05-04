8c7cb594fa98cf1391ff749991bd7883f76656ec
elchudi <gustavogiudici@gmail.com>
2015-05-04 20:05:33 -0300

    added task production for browser that minifies

---
+++ gulp/tasks/production.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var config = require('../config').browserify;
+
+
+gulp.task('production', ['browserify'], function() {
+  return gulp.src(config.bundleConfigs[0].productionFolder + '/' + config.bundleConfigs[0].outputName)
+    // This will minify and rename to foo.min.js
+    .pipe(uglify())
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(gulp.dest(config.bundleConfigs[0].productionFolder));
+});
