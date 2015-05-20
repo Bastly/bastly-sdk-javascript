b5bd61ab1255edbe89bd9da9ae20c1b8d4238406
elchudi <gustavogiudici@gmail.com>
2015-05-20 12:49:13 -0300

    added prodction tasks for node and browser

--- gulp/tasks/production-browser.js
+++ gulp/tasks/production-browser.js
@@ -4,7 +4,7 @@ var uglify = require('gulp-uglify');
 var config = require('../config').browserify;
 
 
-gulp.task('production', ['browserify'], function() {
+gulp.task('production-browser', ['browserify'], function() {
   return gulp.src(config.bundleConfigs[0].productionFolder + '/' + config.bundleConfigs[0].outputName)
     // This will minify and rename to foo.min.js
     .pipe(uglify())
