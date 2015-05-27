696190b9d187b593dc76befed06abc29fbe0d18f
Alex Vidal <goofyahead@gmail.com>
2015-05-27 19:39:16 +0200

    forward params to cb

--- src/node/bastlyZeromq.js
+++ src/node/bastlyZeromq.js
@@ -25,7 +25,7 @@ module.exports = function(opts){
         log.info('got message', parsedResponse);
         var workerIp = parsedResponse.ip;
         //TODO we must implement some way to understand which response is to each request , since the order does not have to be LILO
-        callbacks.shift()({ status: result.toString(), data: parsedResponse });
+        callbacks.shift()({ result.toString(), parsedResponse });
     });
 
     // Send messages sockets
