c03c9ef414ed77be2354972a098ee28f0933bc42
Gustavo <gustavo@bastly.com>
2015-06-06 10:49:16 +0000

    added rename gulp for replacing ../ to ./

--- gulp/tasks/production-node.js
+++ gulp/tasks/production-node.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
+var replace = require('gulp-replace');
+
 var uglify = require('gulp-uglify');
 var config = require('../config').browserify;
 
@@ -7,6 +9,7 @@ gulp.task('production-node', [], function() {
     return gulp.src(['src/bastlyBase.js',
                 'src/node/**'],
             {base: 'src/'})
+        .pipe(replace(/\.\.\//g, './'))
         .pipe(rename({dirname: ''}))
         .pipe(gulp.dest('./build/node'));
 });
